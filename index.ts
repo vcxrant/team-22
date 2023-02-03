821ba48f3030daf3f1a682f6352ce56d907dd447
Hasan <hasansultan92@hotmail.com>
2023-02-02 22:19:40 -0500

    upgrades to API caller

--- index.ts
+++ index.ts
@@ -1,12 +1,25 @@
-
-const axios = require("axios")
+import { Octokit } from "octokit";
 require("dotenv").config();
-try {
-  const secretKey: string = process.env.GITHUB1;
+
+/* try {
+  
   console.log(secretKey);
 } catch (e) {
   console.log(e);
-}
+} */
+
+const secretKey: string = process.env.GITHUB;
+const octokit = new Octokit({
+  auth: secretKey,
+});
+console.log(secretKey);
+
+const averageFunction = (
+  totalContributors: number,
+  totalContributions: number
+): number => {
+  return totalContributions / totalContributors;
+};
 
 const validateUrl = (url: string) => {
   if (
@@ -20,19 +33,113 @@ const validateUrl = (url: string) => {
   }
 };
 
-const getRepoInfo = async (url: string) => {
-  if (validateUrl(url)) {
-    axios
-      .get("https://api.coindesk.com/v1/bpi/currentprice.json")
-      .then(function (response: any) {
-        console.log(response.data);
-      })
-      .catch(function (error: any) {
-        console.error(error);
-      });
-  } else {
-    // error handling for failing the validation stage
-  }
-};
+/* 
+License comparison to be done on another day
+*/
+class repositoryClass {
+  licenses: string;
+  busFactor: number;
+  CorrectNess: number;
+  responsiveMaintainer: number;
+  netScore: number;
+  rampUp: number;
+  packageName: string;
+  commits: number;
+  pullRequest: number;
+  issuesClosed: number;
+  issuesOpen: number;
+  url: string;
+  owner: string;
+  repo: string;
+  contributions: number;
+  numContributors: number;
 
-getRepoInfo("https://api.coindesk.com/v1/bpi/currentprice.json");
+  constructor(repo: string, url: string, owner: string) {
+    this.url = url;
+    this.owner = owner;
+    this.repo = repo;
+    this.contributions = 0;
+  }
+
+  getRepoInfo = async () => {
+    if (validateUrl(this.url)) {
+      let vart = await octokit.request(`GET /repos/{owner}/{repo}/license`, {
+        owner: this.owner,
+        repo: this.repo,
+      });
+      this.licenses = vart.data.license.name;
+      let contributors = await octokit.request(
+        `GET /repos/{owner}/{repo}/contributors`,
+        {
+          owner: this.owner,
+          repo: this.repo,
+          //anon: "1"
+        }
+      );
+      contributors.data.forEach((value, index, array) => {
+        //console.log(value.contributions);
+        this.contributions = value.contributions + this.contributions;
+        this.numContributors = index;
+      });
+      this.numContributors++;
+      const averageContributionrepo: number = averageFunction(
+        this.numContributors,
+        this.contributions
+      );
+      let sum = 0;
+      contributors.data.every((value, index, array) => {
+        sum += value.contributions;
+        console.log(array);
+        if (sum <= averageContributionrepo) {
+          return true;
+        } else if (index == 0 && sum <= averageContributionrepo) {
+          this.busFactor = index + 1;
+          return false;
+        } else {
+          this.busFactor = index + 1;
+          return false;
+        }
+      });
+      console.log(this.busFactor);
+    } else {
+      // error handling for failing the validation stage
+    }
+  };
+  printProperties() {
+    Promise.resolve(
+      console.log(
+        this.licenses,
+        this.busFactor,
+        this.CorrectNess,
+        this.responsiveMaintainer,
+        this.netScore,
+        this.rampUp,
+        this.packageName,
+        this.commits,
+        this.pullRequest,
+        this.issuesClosed,
+        this.issuesOpen,
+        this.url,
+        this.owner,
+        this.repo,
+        this.contributions,
+        this.numContributors
+      )
+    );
+  }
+}
+
+let repo = new repositoryClass(
+  "axios",
+  "https://github.com/axios/axios",
+  "axios"
+);
+console.log(repo.printProperties());
+repo.getRepoInfo();
+console.log(repo.printProperties());
+
+
+
+const customAlgo = () => {};
+
+//getRepoInfo("https://api.coindesk.com/v1/bpi/currentprice.json");
